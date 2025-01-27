87e23d72bc7f94896a2761a1c06398accd85e399
OgnjenPantelic <OgnjenPantelic>
2025-01-27 15:09:05 +0000

    removed permissions to avoid error

---
+++ .bundle/headphones-margin/prod/files/.bundle/headphones-margin/dev/files/src/custom_2.sql
@@ -0,0 +1,9 @@
+-- This query is executed using Databricks Workflows (see resources/headphones-margin_sql.job.yml)
+--
+-- The streaming table below ingests all JSON files in /databricks-datasets/retail-org/sales_orders/
+-- See also https://docs.databricks.com/sql/language-manual/sql-ref-syntax-ddl-create-streaming-table.html
+
+USE CATALOG {{catalog}};
+USE IDENTIFIER({{schema}});
+
+SELECT 2 + 2;
